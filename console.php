97a65d5f4666e88badffda56f80354954e2bad93
rouroumaki <2557654557@qq.com>
2023-08-18 13:43:42 +0800

    test

--- console.php
+++ console.php
@@ -17,7 +17,7 @@
  * @author Victor Dubiniuk <dubiniuk@owncloud.com>
  *
  * @license AGPL-3.0
- *123
+ *
  * This code is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License, version 3,
  * as published by the Free Software Foundation.
